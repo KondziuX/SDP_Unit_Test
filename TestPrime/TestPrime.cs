acf01cc2491a0c2cb24b9d0389d7083bd5d8f83d
KondziuX <ktrybus15@gmail.com>
2022-02-03 21:58:17 +0100

    homework

---
+++ TestPrime/TestPrime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Quality;
+
+namespace TestPrime
+{
+    public class TestPrime
+    {
+        [Test]
+        public void NegativeValueException()
+        {
+            Assert.Throws<ArgumentException>(() => QualityChecker.IsQuality(-5));
+        }
+        [TestCase(0, false)]
+        [TestCase(1, false)]
+        [TestCase(2, true)]
+        [TestCase(3, true)]
+        [TestCase(4, false)]
+        [TestCase(5, true)]
+        [TestCase(6, false)]
+        public void ValueIsPrime(int value, bool expectedValue)
+        {
+            var actualValue = QualityChecker.IsQuality(value);
+            actualValue.Should().Be(expectedValue);
+        }
+    }
+}
